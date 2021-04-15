5dfaa8bc1da38b612c378f03054cafc5b758973d
unknown <ibmjavanh15@iiht.tech>
2021-04-15 13:44:19 +0530

    Added table

--- angular/MyApp/src/app/userform/userform.component.ts
+++ angular/MyApp/src/app/userform/userform.component.ts
@@ -1,14 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from './User';
 
 @Component({
   selector: 'app-userform',
   templateUrl: './userform.component.html',
   styleUrls: ['./userform.component.css']
 })
-export class UserformComponent implements OnInit {
+export class UserformComponent implements OnInit {    //controller
   title: string = 'Userform';
-  firstname:string = 'Ram';
+  user:User = new User(); //model - stores all form data
   constructor() { }
+
+  userArray: User[] = [];
+
+  save(){
+    this.userArray.push(this.user);
+    console.log(this.user.firstname);
+
+    // this.user.firstname = "Rashi";   //value of textbox is changed to Rashi
+
+  }
 
   ngOnInit(): void {
   }
